626835d6092353a6378a72676898df8d79feb716
Steven <stevenhoffman@web.de>
2026-06-22 11:30:46 +0200

    fix: make page wcag conform

--- src/js/htmlTemplates.js
+++ src/js/htmlTemplates.js
@@ -1,14 +1,16 @@
 // return contentHTML-code
 function contentHtml(img, alt) {
     return ` 
-    <img src="../assets/img/content/${img}" alt="${alt}" id="${alt}" onclick="openDialog('${alt}_dialog')" tabindex="0" onkeyup="if(event.key === 'Enter') {openDialog('${alt}_dialog');}">
+    <button type="button" onkeyup="if(event.key === 'Enter') {openDialog('${alt}_dialog');}">
+        <img src="../assets/img/content/${img}" alt="${alt}" id="${alt}" >
+    </button> 
     `
 }
 
 // HTML code for dialogs
 function dialogHtml(i) {
     return `
-    <dialog aria-labelledby="dialogTitle" aria-describedby="dialogDescribtion" id="${contentAlt[i]}_dialog" onkeyup="if(event.key === 'ArrowLeft') {arrowButton(${i}, 'backward')}; if(event.key === 'ArrowRight') {arrowButton(${i}, 'forward')}">
+    <dialog id="${contentAlt[i]}_dialog" onkeyup="if(event.key === 'ArrowLeft') {arrowButton(${i}, 'backward')}; if(event.key === 'ArrowRight') {arrowButton(${i}, 'forward')}">
             <header class="dialog_header">
                 <h2 id="dialogTitle">
                     ${content[i]}
@@ -25,13 +27,13 @@ function dialogHtml(i) {
             </section>
 
             <footer class="dialog_footer">
-                <button class="leftArrowButton" id="leftArrowButton" aria-label="Close button" onclick="arrowButton(${i}, 'backward')">
+                <button class="leftArrowButton" id="leftArrowButton" onclick="arrowButton(${i}, 'backward')">
                     <img class="leftImg" src="../assets/img/navigation/arrow_default.svg" alt="leftArrowButton">
                     <img class="leftImgHover" src="../assets/img/navigation/arrow_hover.svg" alt="leftArrowButton">
                 </button>
 
                 <p>${i + 1}/${content.length}</p>
-                <button class="rightArrowButton" id="rightArrowButton" aria-label="Close button" onclick="arrowButton(${i}, 'forward')">
+                <button class="rightArrowButton" id="rightArrowButton" onclick="arrowButton(${i}, 'forward')">
                     <img class="rightImg" src="../assets/img/navigation/arrow_default.svg" alt="rightArrowButton">
                     <img class="rightImgHover" src="../assets/img/navigation/arrow_hover.svg" alt="rightArrowButton">
                 </button>
